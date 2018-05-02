1f5a96cbb78fd488b9488074cf0b3b2c7fcde7e1
Brent Villanueva <bvillanueva@mdsol.com>
2018-05-02 20:42:53 +0900

    add logger class

--- src/Medidata.Pikapika.Miner/DotnetNugetsMiner.cs
+++ src/Medidata.Pikapika.Miner/DotnetNugetsMiner.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Medidata.Pikapika.Miner.DataAccess;
 using Medidata.Pikapika.Miner.Models;
-using NuGet.Protocol.Core.Types;
 
 namespace Medidata.Pikapika.Miner
 {
@@ -26,27 +24,6 @@ namespace Medidata.Pikapika.Miner
             }
 
             return result;
-        }
-    }
-
-    public class Logger : NuGet.Common.ILogger
-    {
-        public void LogDebug(string data) => Console.WriteLine($"DEBUG: {data}");
-        public void LogVerbose(string data) => Console.WriteLine($"VERBOSE: {data}");
-        public void LogInformation(string data) => Console.WriteLine($"INFORMATION: {data}");
-        public void LogMinimal(string data) => Console.WriteLine($"MINIMAL: {data}");
-        public void LogWarning(string data) => Console.WriteLine($"WARNING: {data}");
-        public void LogError(string data) => Console.WriteLine($"ERROR: {data}");
-        public void LogSummary(string data) => Console.WriteLine($"SUMMARY: {data}");
-
-        public void LogInformationSummary(string data)
-        {
-            Console.WriteLine($"Information Summary: {data}");
-        }
-
-        public void LogErrorSummary(string data)
-        {
-            Console.WriteLine($"Error Summary: {data}");
         }
     }
 }
