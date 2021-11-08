243157889de6520fc085753963d8e71824e844c2
bvillanueva-mdsol <bvillanueva@mdsol.com>
2021-11-08 15:53:19 +0900

    add correct nuget url

--- .12factor/add-nuget-source.sh
+++ .12factor/add-nuget-source.sh
@@ -3,5 +3,5 @@ set -eox pipefail
 
 source env.sh
 
-dotnet nuget add source https://mdsol.jfrog.io/artifactory/api/nuget/nuget-local --name Artifactory \
+dotnet nuget add source https://mdsol.jfrog.io/artifactory/api/nuget/nuget-prod-virtual --name Artifactory \
   --username $ARTIFACTORY_USERNAME --password $ARTIFACTORY_PASSWORD --store-password-in-clear-text
